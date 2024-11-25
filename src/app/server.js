9ab451a1e9e84ec6859eef96e51f63a5a77ee8cd
Bartos Martin <h145323@stud.u-szeged.hu>
2024-11-25 13:17:21 +0100

    projekt inicializálása

---
+++ src/app/server.js
@@ -0,0 +1,28 @@
+/* Konstansok, beimportált elemek */
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const PORT = process.env.PORT || 7500;
+
+const app = express();
+const server = http.createServer(app);
+
+/* Beállítások */
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+/* Alap végpontok kezelése */
+app.get('/', (req, res) => {
+    res.send('Műsorújság projekt');
+});
+
+
+/* Szerver */
+server.listen(PORT, () => {
+    console.log(`A szerver elindult! Port: ${PORT}`);
+});
+
+
