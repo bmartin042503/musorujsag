86b8bbd8ad921c24fe78dc43d938c9223b1f65bc
Bartos Martin <h145323@stud.u-szeged.hu>
2024-11-29 22:15:09 +0100

    program-guide frontend

--- src/app/controllers/ProgramController.js
+++ src/app/controllers/ProgramController.js
@@ -1,6 +1,6 @@
 const getProgramGuide = async(req, res) => {
     try {
-        res.render('program-guide');
+        res.render('program-guide', { channels: channels, availableChannelsCount: 28 });
     } catch(error) {
         res.status(500).json({ message: `Hiba történt a műsorújság betöltése során: ${error}` });
     }
